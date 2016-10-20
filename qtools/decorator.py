e74c2f15dde1544b5b97a517e09b48e7812bd0e8
Maurice Flanagan <mflanagan@circleup.com>
2016-10-20 13:03:42 -0400

    pass in 'cls' instead of unusable 'self' as first parameter to q_method's allow access to q method through class instead of requiring '.q()'

--- qtools/decorator.py
+++ qtools/decorator.py
@@ -1,42 +1,63 @@
-from functools import partial
-
+import types
+import functools
 from django.db.models import Q
+from qtools.filterq import obj_matches_q
 
 
-class ThrowExceptionIfUsed(object):
-    def __getattribute__(self, item):
-        raise Exception('The self parameter cannot be used in methods decorated with q_method.')
+class QMethodCallable(object):
+    def __init__(self, q_func):
+        self.q_func = q_func
+        functools.update_wrapper(self, self.q_func)
 
-
-DO_NOT_USE = ThrowExceptionIfUsed()
-
-
-def q_method(fn):
-    """
-    Decorator for queryset methods that return Q objects.
-
-    Any methods decorated with this must return a Q object.
-
-    The 'self' argument is actually an unusable object but is left there so code checkers don't complain.
-
-    The original function that returns Q objects can be accessed like this:
-
-        class CustomQuerySet(QuerySet):
-
-            @q_method
-            def custom_method(self, limit):
-                return Q(price__gt=a)
-
-        # accessing the q method directly
-        CustomQuerySet.custom_method.q(limit=5)
-    """
-    q_func = partial(fn, DO_NOT_USE)
-
-    def qs_func(self, *args, **kwargs):
-        q = q_func(*args, **kwargs)
+    def q(self, *args, **kwargs):
+        q = self.q_func(*args, **kwargs)
         if not isinstance(q, Q):
             raise ValueError('QuerySet methods decorated with q_method must return a Q object.')
-        return self.filter(q)
+        return q
 
-    qs_func.q = q_func
-    return qs_func
+
+class QInstanceMethodCallable(QMethodCallable):
+    def __call__(self, instance, *args, **kwargs):
+        q = self.q(*args, **kwargs)
+        return instance.filter(q)
+
+
+class QClassMethodCallable(QMethodCallable):
+     def __call__(self, cls, *args, **kwargs):
+        return self.q(*args, **kwargs)
+
+
+class q_method(object):
+    """
+        A decorator that allows you to implement custom QuerySet filters as classmethod's returning a Q object
+
+        class MyQuerySet(QuerySet):
+            def costs_more_than_q(cls, price):
+                return Q(price__gt=price)
+
+            def costs_more_than(self, price):
+                return self.filter(self.costs_more_than_q(price))
+
+        MyModel.objects.costs_more_than(price)
+        MyQuerySet.costs_more_than_q(price)
+
+        vs
+
+        class MyQuerySet(QuerySet):
+            @q_method
+            def costs_more_than(cls, price):
+                return Q(price__gt=price)
+
+        MyModel.objects.costs_more_than(price)
+        MyQuerySet.costs_more_than(price)
+
+    """
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __get__(self, instance, owner):
+        q_func = types.MethodType(self.fn, owner, owner)
+        if instance is not None:
+            return types.MethodType(QInstanceMethodCallable(q_func), instance, owner)
+        else:
+            return types.MethodType(QClassMethodCallable(q_func), owner, owner)
